59aca72b9472b1780b507e2a1cc203d06d95c1a3
mixailvelinov <mihail.vasilev.velinov@gmail.com>
2025-01-31 04:38:15 +0200

    added more solutions

---
+++ number_beggars.py
@@ -0,0 +1,29 @@
+numbers = [int(num) for num in input().split(", ")]
+beggars_count = int(input())
+beggars_results = {}
+list_result = []
+
+
+while numbers:
+    for beggar in range(1, beggars_count + 1):
+
+        if beggar not in beggars_results and len(numbers) == 0:
+            beggars_results[beggar] = [0]
+            continue
+
+        if len(numbers) == 0:
+            break
+
+        elif beggar not in beggars_results:
+            beggars_results[beggar] = [numbers[0]]
+
+        elif beggar in beggars_results:
+            beggars_results[beggar].append(numbers[0])
+
+        del numbers[0]
+
+
+for key, value in beggars_results.items():
+    list_result.append(sum(value))
+
+print(list_result)
